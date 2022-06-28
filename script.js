055df055b58aec3654fb8bbc854e5734ebdbdf89
Jojo <jonathansardeamusa@gmail.com>
2022-06-29 00:45:48 +0800

    corrupted file, redid commit

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ const buttonOperator = buttonBox.getElementsByClassName('button operator');
 const display = document.createElement('div');
 let displayValue = '';
 let secondNum;
-let firstNum;
+let firstNum = '';
+let operatorValue;
+let answer;
 
-// displays value of buttonNumber clicked in display box
 for (const button of buttonNumber) {
     button.addEventListener('click', displayInBox);
 }
@@ -44,21 +45,30 @@ function operate(a,b,operator) {
     }
 }
 
+// displays value of buttonNumber clicked in display box
 function displayInBox(e) {
+    if (answer && secondNum) {
+        displayValue = '';
+        answer = '';
+    }
     displayValue += e.target.textContent;
     display.textContent = displayValue;
     displayBox.appendChild(display);
 }
 
+// store values when operator button is clicked
 function storeValue(e) {
-    if (firstNum === undefined) {
+    if (firstNum === '') {
+        operatorValue = e.target.id;
+        console.log(operatorValue);
         firstNum = displayValue;
         displayValue = '';
-        console.log
     } else {
         secondNum = displayValue;
-        console.log(firstNum,secondNum);
-        displayValue = operate(+firstNum,+secondNum,'add');
+        console.log(firstNum,secondNum,operatorValue);
+        answer = operate(+firstNum,+secondNum,operatorValue);
+        displayValue = answer;
+        firstNum = '';
         console.log(displayValue);
         display.textContent = displayValue;
     }
