139452b0ead4a0d5b4b581d084b4913cd757271e
Jojo <jonathansardeamusa@gmail.com>
2022-06-28 13:54:46 +0800

    added displays to calculator

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const buttonBox = document.getElementById('button-box');
+const displayBox = document.getElementById('display-box');
+const buttons = buttonBox.getElementsByClassName('button');
+const display = document.createElement('div');
+
+// displays value of button clicked in display box
+for (const button of buttons) {
+    button.addEventListener('click',(e) => {
+        const displayValue = e.target.textContent;
+        display.textContent += displayValue;
+        displayBox.appendChild(display);
+    })
+}
+
+// calculator operation functions
 function add(a,b) {
     return a + b;
 }
