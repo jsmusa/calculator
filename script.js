3aa5d3eda7548d002987b9824f2d2a402f8601df
Jojo <jonathansardeamusa@gmail.com>
2022-06-28 17:55:45 +0800

    reworked operate function, compiled calculator function into an object added eventlisteners to number buttons and operator buttons

--- script.js
+++ script.js
@@ -1,35 +1,65 @@
 const buttonBox = document.getElementById('button-box');
 const displayBox = document.getElementById('display-box');
-const buttons = buttonBox.getElementsByClassName('button');
+const buttonNumber = buttonBox.getElementsByClassName('button number');
+const buttonOperator = buttonBox.getElementsByClassName('button operator');
 const display = document.createElement('div');
+let displayValue = '';
+let secondNum;
+let firstNum;
 
-// displays value of button clicked in display box
-for (const button of buttons) {
-    button.addEventListener('click',(e) => {
-        const displayValue = e.target.textContent;
-        display.textContent += displayValue;
-        displayBox.appendChild(display);
-    })
+// displays value of buttonNumber clicked in display box
+for (const button of buttonNumber) {
+    button.addEventListener('click', displayInBox);
+}
+
+for (const button of buttonOperator) {
+    button.addEventListener('click',storeValue)
 }
 
 // calculator operation functions
-function add(a,b) {
-    return a + b;
+const operation = {
+    add(a,b) {
+        return a + b;
+    },
+    subtract(a,b) {
+        return a - b;
+    },
+    multiply(a,b) {
+        return a*b;
+    },
+    divide(a,b) {
+        if (b === 0) return 'ERROR, cannot divide by 0';
+        else return a/b;
+    },
 }
 
-function subtract(a,b) {
-    return a - b;
+// console.log(operate(+'-2',+'9','add'));
+
+function operate(a,b,operator) {
+    switch(operator) {
+        case 'add': return operation.add(a,b);
+        case 'subtract': return operation.subtract(a,b);
+        case 'multiply': return operation.multiply(a,b);
+        case 'divide': return operation.divide(a,b);
+    }
 }
 
-function multiply(a,b) {
-    return a*b;
+function displayInBox(e) {
+    displayValue += e.target.textContent;
+    display.textContent = displayValue;
+    displayBox.appendChild(display);
 }
 
-function divide(a,b) {
-    if (b === 0) return 'ERROR, cannot divide by 0';
-    else return a/b;
-}
-
-function operate(a,b,operation) {
-    return operation(a,b);
+function storeValue(e) {
+    if (firstNum === undefined) {
+        firstNum = displayValue;
+        displayValue = '';
+        console.log
+    } else {
+        secondNum = displayValue;
+        console.log(firstNum,secondNum);
+        displayValue = operate(+firstNum,+secondNum,'add');
+        console.log(displayValue);
+        display.textContent = displayValue;
+    }
 }
