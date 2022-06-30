c641e7262adcde4c62a8ccad734572cfcd346232
Jojo <jonathansardeamusa@gmail.com>
2022-07-01 01:12:53 +0800

    reworked calculator functions used arrays to store numbers instead of strings

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ const buttonClear = buttonBox.querySelector('.clear');
 const display = document.createElement('div');
 const answerDisplay = document.createElement('div');
 const dot = document.querySelector('#dot');
-let secondNum = '';
-let firstNum = '';
+let array = [];
+let arrayTwo = [];
+let secondNum;
+let firstNum;
 let operatorValue;
 let answer;
 
 for (const button of buttonNumber) {
-    button.addEventListener('click', displayInBox);
-    
+    button.addEventListener('click', displayInBox);    
 }
 
 for (const button of buttonOperator) {
@@ -21,9 +22,11 @@ for (const button of buttonOperator) {
 }
 
 buttonClear.addEventListener('click',() => {
-    firstNum = secondNum = '';
-    operatorValue = undefined;
-    display.textContent = answerDisplay.textContent = '';
+    firstNum = secondNum = null;
+    operatorValue = null;
+    array = [];
+    attayTwo = [];
+    display.textContent = '';
 })
 
 // calculator operation functions
@@ -54,43 +57,59 @@ function operate(a,b,operator) {
 
 // displays value of buttonNumber clicked in display box
 function displayInBox(e) {
-    if (!operatorValue) {
-        // clears firstNum when operatorValue is falsy, answer is true
-        // and a number button is clicked
-        if (answer) {
-            firstNum = '';
-            answer = '';
-        }
-        firstNum += e.target.textContent;
-        display.textContent = firstNum;
-    } else {
-        secondNum += e.target.textContent;
-        display.textContent +=`${e.target.textContent}`;
-    }
+    // if (!operatorValue) {
+    //     // clears firstNum when operatorValue is falsy, answer is true
+    //     // and a number button is clicked
+    //     if (answer) {
+    //         firstNum = '';
+    //         answer = '';
+    //     }
+    //     firstNum += e.target.textContent;
+    //     display.textContent = firstNum;
+    // } else {
+    //     secondNum += e.target.textContent;
+    //     display.textContent +=`${e.target.textContent}`;
+    // }
 
-    if (e.target.id === 'dot') {
-        e.target.removeEventListener('click',displayInBox);
+    // if (e.target.id === 'dot') {
+    //     e.target.removeEventListener('click',displayInBox);
+    // }
+
+    if (!operatorValue) {
+        firstNum = numberWrite(array,firstNum,e);
+        display.textContent = firstNum;
+        if (array.includes('.')) dot.removeEventListener('click',displayInBox);
+    } else if (operatorValue) {
+        secondNum = numberWrite(arrayTwo,secondNum,e);
+        display.textContent =`${firstNum} ${operatorValue} ${secondNum}`;
+        if (arrayTwo.includes('.')) dot.removeEventListener('click',displayInBox);
     }
 
     displayBox.appendChild(display);
+    // console.log(number,firstNum);
 }
 
 // store values when operator button is clicked
 function storeValue(e) {
     // operate when secondNum is defined and an operator button is clicked
-    if (secondNum && secondNum !== '.') {
+    if (secondNum) {
         answer = operate(+firstNum,+secondNum,operatorValue);
         if (typeof answer === 'number') firstNum = answer;
-        secondNum = '';
-        answerDisplay.textContent = answer;
+        number = [];
+        numberTwo = [];
         // displayBox.appendChild(answerDisplay);        
     }
 
-    if (firstNum && firstNum !== '.' && secondNum !== '.') {
+    if (firstNum) {
     operatorValue = e.target.id;
     display.textContent =`${firstNum} ${operatorValue} `;
-    
-    // adds dot button for second number
+    // // adds dot button for second number
     dot.addEventListener('click',displayInBox);
     }
+    // console.log(operatorValue);
+}
+
+function numberWrite(array,number,event) {
+    array.push(event.target.textContent);
+    return number = array.join('');
 }
