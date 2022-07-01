3c2249cd8957785cb0eee8e237b7d0e53f9662f8
Jojo <jonathansardeamusa@gmail.com>
2022-07-01 16:06:19 +0800

    added conditionals in function numberWrite

--- script.js
+++ script.js
@@ -57,32 +57,12 @@ function operate(a,b,operator) {
 
 // displays value of buttonNumber clicked in display box
 function displayInBox(e) {
-    // if (!operatorValue) {
-    //     // clears firstNum when operatorValue is falsy, answer is true
-    //     // and a number button is clicked
-    //     if (answer) {
-    //         firstNum = '';
-    //         answer = '';
-    //     }
-    //     firstNum += e.target.textContent;
-    //     display.textContent = firstNum;
-    // } else {
-    //     secondNum += e.target.textContent;
-    //     display.textContent +=`${e.target.textContent}`;
-    // }
-
-    // if (e.target.id === 'dot') {
-    //     e.target.removeEventListener('click',displayInBox);
-    // }
-
     if (!operatorValue) {
-        firstNum = numberWrite(array,firstNum,e);
+        firstNum = numberWrite(array,e);
         display.textContent = firstNum;
-        if (array.includes('.')) dot.removeEventListener('click',displayInBox);
     } else if (operatorValue) {
-        secondNum = numberWrite(arrayTwo,secondNum,e);
+        secondNum = numberWrite(arrayTwo,e);
         display.textContent =`${firstNum} ${operatorValue} ${secondNum}`;
-        if (arrayTwo.includes('.')) dot.removeEventListener('click',displayInBox);
     }
 
     displayBox.appendChild(display);
@@ -95,8 +75,9 @@ function storeValue(e) {
     if (secondNum) {
         answer = operate(+firstNum,+secondNum,operatorValue);
         if (typeof answer === 'number') firstNum = answer;
-        number = [];
-        numberTwo = [];
+        secondNum = null;
+        array = [];
+        arrayTwo = [];
         // displayBox.appendChild(answerDisplay);        
     }
 
@@ -109,7 +90,23 @@ function storeValue(e) {
     // console.log(operatorValue);
 }
 
-function numberWrite(array,number,event) {
+function numberWrite(array,event) {
+    // prevents more than one dot from being included
+    if (array.includes('.') && event.target.id === 'dot') {
+        return array.join('');
+    }
+
+    // doesn't allow numbers that start with 0 unless it has a decimal point
+    if (array[0] === '0' && !array.includes('.')) {
+        array.pop();
+    }
+    
     array.push(event.target.textContent);
-    return number = array.join('');
+    
+    // adds 0 to the start if decimal point is the first in the array
+    if (array[0] === '.') {
+        array.unshift('0');
+    }
+
+    return array.join('');
 }
